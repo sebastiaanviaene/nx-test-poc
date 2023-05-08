9ccc61d5291509132bc01a328fd72fa371c11db0
sebastiaanviaene <sebviaene@gmail.com>
2023-05-08 16:55:33 +0200

    init

---
+++ packages/tests/jest.config.ts
@@ -0,0 +1,6 @@
+/* eslint-disable */
+export default {
+  displayName: 'tests',
+  preset: '../../jest.preset.js',
+  coverageDirectory: '../../coverage/packages/tests',
+};
---
+++ packages/tests/src/sample.test.ts
@@ -0,0 +1,8 @@
+import { lol } from 'packages/app3/src/main';
+
+describe('lol', () => {
+  it('should test smtn', () => {
+    const haha = lol();
+    console.log({ haha });
+  });
+});
